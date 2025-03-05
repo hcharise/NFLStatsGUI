561defde731bcefbfe158c6544c60f5360ad472e
hcharise <110205350+hcharise@users.noreply.github.com>
2025-03-05 10:06:17 -0700

    Add project files.

---
+++ NFLStatsGUI/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+
+namespace MyWPFApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ProcessButton_Click(object sender, RoutedEventArgs e)
+        {
+            string input = InputTextBox.Text;
+
+            OutputTextBlock.Text = "Hello!";
+        }
+    }
+}
